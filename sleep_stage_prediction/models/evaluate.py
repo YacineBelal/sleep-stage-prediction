6760d851bfcba5dcc4fc9d3135148074d666ea0a
Yacine Belal <yacine.belal@cea.fr>
2026-03-13 13:22:47 +0100

    add f1-score metric

--- sleep_stage_prediction/models/evaluate.py
+++ sleep_stage_prediction/models/evaluate.py
@@ -1,10 +1,11 @@
+from sklearn.metrics import f1_score
 import torch
 from torch.utils.data import DataLoader
 
 from sleep_stage_prediction.data import DreamtDataset
 
 
-def test_model(model, X_test, y_test, criterion, batch_size=256, device="cpu"):
+def test_model(model, X_test, y_test, criterion, batch_size=256, device=torch.device("cpu")):
 
     # TODO refactor dataloaders creation from train and evaluate
     test_ds_clients = [DreamtDataset(x, y) for x, y in zip(X_test, y_test)]
@@ -13,19 +14,25 @@ def test_model(model, X_test, y_test, criterion, batch_size=256, device="cpu"):
     ]
     generalization_error = []
     accuracy = []
+    f1_scores = []
     for test_dl in test_dl_clients:
-        gen_err, acc = _test_model(model, test_dl, criterion, batch_size, device)
+        gen_err, acc, score = _test_model(model, test_dl, criterion, batch_size, device)
         generalization_error.append(gen_err)
         accuracy.append(acc)
+        f1_scores.append(score)
 
     generalization_error = sum(generalization_error) / len(generalization_error)
     accuracy = sum(accuracy) / len(accuracy)
+    score = sum(f1_scores) / len(f1_scores)
 
-    print(f"Generalization Error:{generalization_error}, Accuracy {accuracy}")
-    return generalization_error, accuracy
+    return {
+        "Generalization Error": {generalization_error},
+        "Accuracy": {accuracy},
+        "F1-Score": {score},
+    }
 
 
-def _test_model(model, test_dl, criterion, batch_size=256, device="cpu"):
+def _test_model(model, test_dl, criterion, batch_size=256, device=torch.device("cpu")):
     generalization_error = 0.0
     correct = 0
     all_preds = []
@@ -44,9 +51,11 @@ def _test_model(model, test_dl, criterion, batch_size=256, device="cpu"):
             all_preds.append(pred.cpu())
             all_targets.append(y.cpu())
 
-        y_pred = torch.cat(all_preds).numpy()
-        y_true = torch.cat(all_targets).numpy()
+        y_pred = torch.cat(all_preds)
+        y_true = torch.cat(all_targets)
         generalization_error /= len(test_dl.dataset)
         accuracy = correct / len(test_dl.dataset)
 
-    return generalization_error, accuracy
+    f1score = f1_score(y_true, y_pred, average="macro")
+
+    return generalization_error, accuracy, f1score
